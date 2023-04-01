57742c6874010159bfd4cb3e3cc7d355398a118f
PiotrMei <meipiotr@gmail.com>
2023-04-01 18:36:46 +0200

    Database Update

---
+++ ScheduleApp/ScheduleApp/Migrations/20230401162325_AddClientId.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScheduleApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddClientId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ClientId",
+                table: "Adresses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClientId",
+                table: "Adresses");
+        }
+    }
+}
