cbe02e285765b1c066c6943b69603f317da00b1e
tommei <tomasz.meihsner@gmail.com>
2023-04-25 19:44:45 +0200

    Fixed ScheduleMappingProfile - added public constructor.

--- ScheduleApp/ScheduleUI/Middleware/ErrorHandlingMiddleware.cs
+++ ScheduleApp/ScheduleUI/Middleware/ErrorHandlingMiddleware.cs
@@ -4,6 +4,12 @@ namespace ScheduleCore.Middleware
 {
     public class ErrorHandlingMiddleware : IMiddleware
     {
+        private readonly ILogger _logger;
+        public ErrorHandlingMiddleware(ILogger<ErrorHandlingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
             try
@@ -21,11 +27,11 @@ namespace ScheduleCore.Middleware
                 context.Response.StatusCode = 403;
                 await context.Response.WriteAsync(forbidden.Message);
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogError(ex, "Internal server error");
                 context.Response.StatusCode = 500;
                 await context.Response.WriteAsync("Something went wrong");
-
             }
         }
     }
